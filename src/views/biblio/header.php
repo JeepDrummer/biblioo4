3b448d3819a91df55d6f38722eae673f9de37959
Kreolia <fredmouthe@gmail.com>
2017-12-08 11:13:24 +0100

    templates and assets

---
+++ src/views/biblio/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Biblioo Cefim</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="/web/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/web/assets/css/mdb.min.css" rel="stylesheet">
+    <link href="/web/assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg justify-content-between navbar-light grey lighten-2">
+    <a class="navbar-brand" href="#"><img src="./../web/assets/img/logo.png" class="img-fluid" alt="logo"></a>
+    <form class="form-inline">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+    </form>
+</nav>
